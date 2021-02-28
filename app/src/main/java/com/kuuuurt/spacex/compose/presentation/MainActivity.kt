b95e8f3e65b48fc2fd075af8a9314104cdac9fb5
Kurt Renzo Acosta <18444033+kuuuurt@users.noreply.github.com>
2021-02-28 18:03:15 +0800

    Adds bottom navigation

--- app/src/main/java/com/kuuuurt/spacex/compose/presentation/MainActivity.kt
+++ app/src/main/java/com/kuuuurt/spacex/compose/presentation/MainActivity.kt
@@ -2,16 +2,25 @@ package com.kuuuurt.spacex.compose.presentation
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.*
+import com.kuuuurt.spacex.compose.R
 import com.kuuuurt.spacex.compose.presentation.features.launches.Launches
 import com.kuuuurt.spacex.compose.presentation.theme.SpaceXTheme
 
@@ -26,14 +35,55 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+enum class Sections(
+    val route: String,
+    @DrawableRes val icon: Int
+) {
+    Launches("Launches", R.drawable.vector_rocket_launch),
+    Rockets("Rockets", R.drawable.vector_rocket)
+}
+
+private val Destinations = listOf(
+    Sections.Launches,
+    Sections.Rockets
+)
+
 @Composable
 fun Home() {
+    val navController = rememberNavController()
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text("SpaceX") },
             )
         },
+        bottomBar = {
+            BottomNavigation {
+                val currentRoute = currentRoute(navController)
+                Sections.values().forEach {
+                    BottomNavigationItem(
+                        icon = {
+                            Image(
+                                painter = painterResource(id = it.icon),
+                                contentDescription = it.name,
+                                colorFilter = ColorFilter.tint(MaterialTheme.colors.secondary)
+                            )
+                        },
+                        label = { Text(it.route) },
+                        selected = currentRoute == it.route,
+                        onClick = {
+                            // This if check gives us a "singleTop" behavior where we do not create a
+                            // second instance of the composable if we are already on that destination
+                            if (currentRoute != it.route) {
+                                navController.navigate(it.route)
+                            }
+                        },
+                        selectedContentColor = Color.White,
+                        unselectedContentColor = Color.Gray
+                    )
+                }
+            }
+        }
     ) {
         Box(
             modifier = Modifier.padding(
@@ -43,7 +93,20 @@ fun Home() {
                 end = it.calculateEndPadding(LayoutDirection.Ltr)
             )
         ) {
-            Launches()
+            NavHost(navController, startDestination = Sections.Launches.route) {
+                composable(Sections.Launches.route) {
+                    Launches()
+                }
+                composable(Sections.Rockets.route) {
+                    Text("Rockets!!!")
+                }
+            }
         }
     }
+}
+
+@Composable
+private fun currentRoute(navController: NavHostController): String? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    return navBackStackEntry?.arguments?.getString(KEY_ROUTE)
 }
